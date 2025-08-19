12c3e28ac8cc0ce81a6b0a19ce5cd4d324f92230
vijaylaxmi <vijaylaxmi@atos.net>
2025-08-19 19:17:22 +0530

    First commit of jlc bookstore

---
+++ src/main/java/com/study/maven/MyjlcBookstoreMavenApplication.java
@@ -0,0 +1,13 @@
+package com.study.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjlcBookstoreMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjlcBookstoreMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/maven/MyjlcBookstoreMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjlcBookstoreMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
